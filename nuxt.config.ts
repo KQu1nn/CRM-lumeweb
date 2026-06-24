32a77bcba8a6bedc72cc7015793c90baf4c5c256
KQu1nn <themastertechbr@gmail.com>
2026-06-24 13:10:16 -0300

    netlify branch added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,5 +22,9 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap' }
       ]
     }
+  },
+
+  nitro: {
+    preset: 'netlify'
   }
 })
